63e38f976987ff2c1a0a12cfd74a216a68712d30
SoumitraAgarwal <agarwalsoumitra1504@gmail.com>
2017-05-12 20:30:03 +0530

    Random test commit

--- Check/check_minor.py
+++ Check/check_minor.py
@@ -1,12 +1,34 @@
 import cv2
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 
 img = cv2.imread("test.png", 0)
 median = cv2.medianBlur(img ,5)	
 dst = cv2.fastNlMeansDenoising(median,None,10,7,21)
 ret,thresh2 = cv2.threshold(dst,230,255,cv2.THRESH_BINARY)
-# laplacian = cv2.Laplacian(dst,cv2.CV_64F)
-# sobelx = cv2.Sobel(dst,cv2.CV_64F,1,0,ksize=5)
-# edges = cv2.Canny(laplacian,100,200)
-cv2.imwrite("result.png", thresh2)
+
+
+maxes = []
+name = []
+
+dirs = os.listdir("Test")
+
+
+# All the 6 methods for comparison in a list
+methods = 'cv2.TM_CCOEFF_NORMED'
+
+
+template1 = cv2.imread("Test/3.png",0)
+template2 = cv2.imread("Test/S.png",0)
+
+method = eval(methods)
+# cv2.imwrite("thresh.jpg", thresh2)
+# thresh2 = cv2.imread("thresh.jpg")
+res = cv2.matchTemplate(thresh2,template1,method)
+res = res*255
+print(res.shape)
+cv2.imwrite("element.jpg",res)
+
+# print(maxes)
+# print(name)
